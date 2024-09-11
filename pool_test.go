56e3074ae334af6f949dd20bf4d53a118f921770
Muhamad Sirojudin <muhamad.sirojudin@hyperscal.com>
2024-09-11 16:59:01 +0700

    test sync pool with default value

--- pool_test.go
+++ pool_test.go
@@ -8,7 +8,11 @@ import (
 )
 
 func TestPool(t *testing.T) {
-	pool := sync.Pool{}
+	pool := sync.Pool{
+		New: func() interface{} {
+			return "New"
+		},
+	}
 
 	pool.Put("Izz")
 	pool.Put("Luthfi")
