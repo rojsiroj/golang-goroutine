0a1b6a91e07305f659818fc67a5a72d9ba63374f
Muhamad Sirojudin <muhamad.sirojudin@hyperscal.com>
2024-09-11 15:11:57 +0700

    test rw mutex for race condition in read and write

---
+++ rwmutex_test.go
@@ -0,0 +1,41 @@
+package golanggoroutine
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+type BankAccount struct {
+	RWMutex sync.RWMutex
+	Balance int
+}
+
+func (account *BankAccount) AddBalance(amount int) {
+	account.RWMutex.Lock()
+	account.Balance = account.Balance + amount
+	account.RWMutex.Unlock()
+}
+
+func (account *BankAccount) GetBalance() int {
+	account.RWMutex.RLock()
+	balance := account.Balance
+	account.RWMutex.RUnlock()
+	return balance
+}
+
+func TestRwMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 0; i < 1000; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+	}
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total Balance: ", account.GetBalance())
+}
