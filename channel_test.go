f8ca3ccb6e7076a53fbba5c08cda70bc8923284a
Muhamad Sirojudin <muhamad.sirojudin@hyperscal.com>
2024-09-11 14:38:23 +0700

    test select channel

--- channel_test.go
+++ channel_test.go
@@ -100,3 +100,29 @@ func TestRangeChannel(t *testing.T) {
 
 	fmt.Println("Selesai")
 }
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeRespone(channel1)
+	go GiveMeRespone(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari Channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+}
