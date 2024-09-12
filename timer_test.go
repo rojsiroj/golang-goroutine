65217f6a022e660812b43b3ca0bfef0ba9a8b289
Muhamad Sirojudin <muhamad.sirojudin@hyperscal.com>
2024-09-12 09:38:03 +0700

    test timer for goroutine

---
+++ timer_test.go
@@ -0,0 +1,40 @@
+package golanggoroutine
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestTimer(t *testing.T) {
+	timer := time.NewTimer(5 * time.Second)
+	fmt.Println(time.Now())
+
+	time := <-timer.C
+	fmt.Println(time)
+}
+
+func TestTimerAfter(t *testing.T) {
+	// only pass channel, not timer object
+	channel := time.After(5 * time.Second)
+	fmt.Println(time.Now())
+
+	time := <-channel
+	fmt.Println(time)
+}
+
+func TestAfterFunc(t *testing.T) {
+	group := sync.WaitGroup{}
+	group.Add(1)
+	fmt.Println(time.Now())
+
+	time.AfterFunc(5*time.Second, func() {
+		fmt.Println("Execute after 5 second")
+		fmt.Println(time.Now())
+		group.Done()
+	})
+
+	group.Wait()
+	fmt.Println(time.Now())
+}
